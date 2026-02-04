5228e9a93cae2d15f1cd498432415ad83ffe0850
Subhang <subhangmall10@gmail.com>
2026-02-04 11:43:53 -0800

    added (not currently functional) initial SVGA capabilities

--- pmodekernel/16bitdrv/vgaInit.c
+++ pmodekernel/16bitdrv/vgaInit.c
@@ -5,27 +5,83 @@
 #include <i86.h>
 #include <malloc.h>
 
-void setPalette(unsigned char colorPalette[16][3]);
+#pragma pack(push, 1)
+struct VbeInfoBlock {
+    char vbeSignature[4];
+    uint16_t vbeVersion;
+    uint16_t oemStringPtr[2];
+    uint8_t capabilities[4];
+    uint16_t videoModePtr[2]; // ptr to modes
+    uint16_t totalMemory;
+    uint8_t reserved[492];
+};
+#pragma pack(pop)
+
+uint16_t* modes;
 
 bool checkVGACompatibility() {
     uint16_t axAfter;
 
-    __asm {
-        mov ax, 0x4F00
-        int 0x10
-        mov axAfter, ax
-    }
+    asm volatile (
+        "mov 0x4F00, %%ax\n\t"
+        "int 0x10\n\t"
+        "mov %%ax, %0"
+        : "=r" (axAfter)
+        :
+        : "ax"
+    );
 
     return (axAfter == 0x004F);
 }
 
+struct VbeInfoBlock *v;
+
+// check if SVGA supported and if it is initialize the VgaInfoBlock
+bool getSVGACompatibility() {
+    uint16_t axOutput;
+    v = calloc(512, 1);
+    unsigned short segment = (unsigned short)((unsigned long)&v >> 16);
+    unsigned short offset = (unsigned short)((unsigned long)&v & 0xFFFF);
+    
+    // load SVGA modes into v
+    asm volatile (
+        "mov 0x4F00, %%ax\n\t"
+        "mov %%1, %%es\n\t"
+        "mov %%2, %%di\n\t"
+        "int 0x10\n\t"
+        "mov %%ax, %%0"
+        : "=r" (axOutput)
+        : "m" (segment), "m" (offset)
+        : "ax", "di"
+    );
+
+    // if didn't return 0x004F, it means that it's not supported
+    if (!(axOutput == 0x004F)) {
+        return false;
+    }
+    return true;
+}
+
+// WORK ON LATER: FIGURE OUT A WAY TO SETUP A FAR POINTER IN GCC
+bool initSVGAModes() {
+    if (v == NULL && !getSVGACompatibility()) {
+        return false;
+    }
+
+    uint32_t linearAddress = (v->videoModePtr[1] << 16) + v->videoModePtr[0];  
+    modes = linearAddress;
+}
+
 
 void initVideoMode(unsigned char pal[16][3]) {
-    __asm {
-        mov ah, 0x00 // Set video mode
-        mov al, 0x12 // 640x200 pixel graphics, 16 colors
-        int 0x10 // Call BIOS video interrupt
-    }
+    asm volatile (
+        "mov 0x00, %%ah\n\t" // Set video mode
+        "mov 0x12, %%al\n\t" // 640x200 pixel graphics, 16 colors
+        "int 0x10" // Call BIOS video interrupt
+        :
+        :
+        : "ax"
+    );
     
     setPalette(pal);
-}
+} 
--- pmodekernel/kernel.c
+++ pmodekernel/kernel.c
@@ -1,6 +1,5 @@
-void setPalette(unsigned char colorPalette[16][3]);
-void initGraphics();
+#include "./pmodedrv/vga.h"
 
 void kmain(void) {
-
+    
 }
